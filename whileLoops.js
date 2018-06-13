234822e5c261b065c1beaa8ffc8580618a72941c
Romero Lijkwan <romerolijkwan@gmail.com>
2018-06-13 17:17:47 +0200

    Update whileLoops.js

--- whileLoops.js
+++ whileLoops.js
@@ -17,7 +17,6 @@
 //    x--;
 //}
 //
-
 //Print all numbers between -10 and 19
 console.log('Print all numbers between -10 and 19');
 var x = -10;
@@ -26,7 +25,6 @@ while(x <= 19) {
     console.log(x);
     x += 1
 }
-
 //Print all even numbers between 10 and 40
 console.log('Print all even numbers between 10 and 40');
 var x = 10;
@@ -35,7 +33,6 @@ while(x <= 40) {
     console.log(x);
     x += 2;
 }
-
 //Print all odd numbers between 300 and 333
 console.log('Print all odd numbers between 300 and 333');
 var x = 301;
@@ -44,7 +41,6 @@ while(x <= 333) {
     console.log(x);
     x += 2;
 }
-
 //Print all numbers divisible by 5 and 3 between 5 and 50
 console.log('Print all numbers divisible by 5 and 3 between 5 and 50');
 var x = 1;
